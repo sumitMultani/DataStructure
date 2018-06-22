47a6600faba90047b7b8d64badf9ef8e2bffb2c4
sumitpal.singh <sumitpal.singh@cloudsmartz.net>
2018-06-22 19:25:37 +0530

    code changed

--- src/com/example/Array/OneDeminisionalArray.java
+++ src/com/example/Array/OneDeminisionalArray.java
@@ -9,11 +9,15 @@ public class OneDeminisionalArray {
 		a[2] = 70;
 		a[3] = 40;
 		a[4] = 50;
+		
+		int b[] = new int[]{1,2,4,5,8,9,6,3,7};
 
 		// printing array
 		for (int i = 0; i < a.length; i++)
 			// length is the property of array
 			System.out.println(a[i]);
+			System.out.println("a -> length : "+ a.length);
+			System.out.println("b -> length : "+ b.length);
 
 	}
 }
